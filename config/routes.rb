75256bc453c51e240a88e1d9f2e04fcca914bf38
Matthew Moore <matt@thrivesmart.com>
2009-11-21 16:51:47 -0800

    items db part finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'home'
   map.resources :alphabetical_items
+  map.resources :store_location_items
+  map.resources :vendor_supply_type_items
+  map.resources :country_items
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
